2c24b62c4fb7e04edd58466625fdeec0c192f43f
zeande <zechariah.andersen@gmail.com>
2013-01-23 18:14:14 -0600

    Added some methods to the Interactable interface.

--- src/strain/ui/Interactable.java
+++ src/strain/ui/Interactable.java
@@ -2,10 +2,16 @@ package strain.ui;
 
 public interface Interactable {
 	
+	public void initialize();
+	
 	public int getChoice(int min, int max);
 	
 	public int getChoice(String message, int min, int max);
 	
 	public String getResponse(String message);
+	
+	public void switchToIntroScreen();
+	
+	public void switchToGameScreen();
 	
 }
