b2b6d6e4696f9122c2411218f36ace5a77d9c874
zeande <zechariah.andersen@gmail.com>
2013-01-26 10:08:12 -0600

    Changed Player class to Character class and added the PlayerCharacter class.

---
+++ src/strain/entity/Character.java
@@ -0,0 +1,59 @@
+package strain.entity;
+
+import java.util.ArrayList;
+import strain.exception.OrganismIncompleteException;
+import strain.exception.OrganismNotInPlayException;
+import strain.tile.Hand;
+import strain.tile.Organism;
+
+public abstract class Character {
+	
+	private int score;
+	private Hand hand;
+	private ArrayList<Organism> organismsInPlay;
+	private ArrayList<Organism> completedOrganisms;
+	public String name;
+	
+	public Character(String name) {
+		this.hand = new Hand();
+		this.name = name;
+		this.score = 0;
+		this.organismsInPlay = new ArrayList<Organism>();
+		this.completedOrganisms = new ArrayList<Organism>();
+	}
+	
+	public abstract void declareBattle(Character opponent);
+	
+	public abstract void endTurn();
+	
+	/**
+	 * Scores the specified organism tile.
+	 * @param tile The organism to score.
+	 * @return True if the player has won the game; otherwise, false.
+	 * @throws OrganismIncompleteException if the specified organism
+	 * 		is not yet complete.
+	 * @throws OrganismNotInPlayException if the specified organism is not in
+	 * 		play. 
+	 */
+	public boolean scoreOrganism(Organism tile) throws 
+			OrganismIncompleteException, OrganismNotInPlayException {
+		
+		if (!tile.isComplete())
+			throw new OrganismIncompleteException("This organism is not yet c" +
+					"omplete! Complete organism before scoring it.");
+		if (!organismsInPlay.contains(tile))
+			throw new OrganismNotInPlayException("Error! This organism is not" +
+					" in play!");
+		
+		score += tile.getVictoryPoints();
+		//tile.clear
+		organismsInPlay.remove(tile);
+		completedOrganisms.add(tile);
+		
+		return score >= 12;
+	}
+	
+	public Hand getHand() {
+		return hand;
+	}
+}
---
+++ src/strain/entity/PlayerCharacter.java
@@ -0,0 +1,21 @@
+package strain.entity;
+
+public class PlayerCharacter extends Character {
+
+	public PlayerCharacter(String name) {
+		super(name);
+	}
+
+	@Override
+	public void declareBattle(Character opponent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void endTurn() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
